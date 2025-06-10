e4250219e04d3d7e3a02b23fea91c72f72e2864c
Aaditya Neupane <aaditya.neupane@stud.th-deg.de>
2025-06-11 00:10:23 +0200

    encoded and data splitted

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,10 @@
 import os
 import pandas as pd
 import cv2
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
 
 image_paths = []
 labels = []
@@ -55,5 +59,36 @@ print(df["resized_image"][1].shape)
 cv2.imshow('Before Resize', df["raw_image"][2])
 cv2.imshow('After Resize', df["resized_image"][2])
 
+
+
+# converting the resized_image column to a single np array
+x= np.array(df['resized_image'].tolist())
+y= df['label']
+
+# normalization of pixel values
+x = x.astype('float32')/255.0
+
+#encoding the labels
+enco = LabelEncoder()
+y_encoded = enco.fit_transform(y)
+y_categorical = to_categorical(y_encoded)
+
+
+#split data
+x_train, x_val, y_train, y_val = train_test_split(x,y_categorical, test_size=0.2, random_state=12, stratify=y_encoded)
+
+
+print("x train", x_train[0])
+print("y train", y_train[0])
+
+
+
+
+
+
+
+
+
+
+
 cv2.waitKey(0)
-#finding the smallest image
